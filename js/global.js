84b62c1bb2181cb596b99f294f3d67464054f79e
Kubi <trungn3@uci.edu>
2014-04-20 09:31:44 -0700

    Modified The UI

--- js/global.js
+++ js/global.js
@@ -4,78 +4,33 @@
 
  var srcHead = "http://api.voicerss.org/?key=8aeeba23cecf4c36a423044b3e6237f6&src=",
      srcTail = "&hl=en-gb",
-
      sourceHead = '<source src="',
-
      sourceTail = ' ">',
 
-     $goBtn = $("#goBtn"),
-
+     /* Set of buttons */
+     $goBtn = $("#go"),
      $verbBtn = $(".btnVerb"),
-
      $nounBtn = $(".btnNoun"),
 
-     $result = $(".result");
+     /* Result Field */
+     $result = $(".result"),
+
+    /* Check if already speak */
+     $checkIfSpeak = false;
+     ;
 
 
-/*
-<audio id="beep-one" controls="controls" preload="auto">
-        <source src="audio/beep.mp3">
-        <source src="audio/beep.ogg">
-        Your browser isn't invited for super fun time.
-      </audio>
-*/
-
-
-
-$verbBtn.click(function(e) {
-
-  var $this = $(this),
-      currentResult = "";
-
-
-  // Store the current text in the result
-  $currentResult = $result.text();
-
-
-  // Store the word
-  keywords = $this.data("tts-value");
-
-  $currentResult = $currentResult + keywords + " ";
-
-  $currentResult = $currentResult.replace("+"," ");
-
-  $result.text($currentResult);
-
-
-  e.preventDefault();
+/* Add keywords to result field when press on verb or noun button */
+$verbBtn.click(function() {
+  addKeywordToResult($(this));
 });
 
-$nounBtn.click(function(e) {
-
-  var $this = $(this),
-      currentResult = "";
-
-
-  // Store the current text in the result
-  $currentResult = $result.text();
-
-
-  // Store the word
-  keywords = $this.data("tts-value");
-
-  $currentResult = $currentResult + keywords + " ";
-
-  $currentResult = $currentResult.replace("+"," ");
-
-  $result.text($currentResult);
-
-
-  e.preventDefault();
+$nounBtn.click(function() {
+  addKeywordToResult($(this));
 });
 
 
-/* Go Button */
+/* Go Button, when press -> make sound */
 $goBtn.click(function(e) {
   var $sound = $("#sound"),
       src = "",
@@ -85,17 +40,48 @@ $goBtn.click(function(e) {
   // Clear all previous sounds
   $sound.empty();
 
+  // Build up the API Link
   src = srcHead + $result.text() + srcTail;
 
+  // Create audio object
   audio = sourceHead + src + sourceTail;
 
+  // Append the object to the DOM
   $sound.append(audio);
+
+  // Make it play sound
   $('#sound')[0].play();
 
-  // Clear the result box
-  $result.empty();
+  // Check speed
+  $checkIfSpeak = true;
 
   e.preventDefault();
 });
 
+
+/* Add keywords to result */
+function addKeywordToResult(element, e){
+  var $element = element,
+      currentResult = "";
+
+  if($checkIfSpeak){
+    // Clear the result box
+    $result.empty();
+    $checkIfSpeak = false;
+  }
+
+  // Store the current text in the result
+  $currentResult = $result.text();
+
+
+  // Store the word
+  keywords = $element.data("tts-value");
+
+  $currentResult = $currentResult + keywords + " ";
+
+  $currentResult = $currentResult.replace("+"," ");
+
+  $result.text($currentResult);
+
+}
 
